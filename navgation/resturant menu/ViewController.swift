7ec6cf178cb4639916e136b37263ff184c87043e
sara al zhrani <saraalzhrani@MacBook-Pro-alkhas-b-sara.local>
2021-10-06 18:53:32 +0300

    navigation

---
+++ navgation/resturant menu/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  resturant menu
+//
+//  Created by sara al zhrani on 29/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lable: UILabel!
+    
+    @IBOutlet weak var butaction: UIButton!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+       // lable.text = NSLocalizedString("good morning", comment: "" )
+        
+       // butaction.setTitle(NSLocalizedString("change language", comment: "", for : .normal)
+                           
+//}
+
+}
+

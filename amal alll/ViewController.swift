7e3e0452b2239df6715950aa17b79ad80a9f5139
amal <amal.141726@gmail.com>
2021-10-06 17:09:57 +0300

    lab9

---
+++ amal alll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  amal alll
+//
+//  Created by Dr. Fahad on 29/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

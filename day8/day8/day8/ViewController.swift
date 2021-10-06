214bcf4b8447259ee9f3bd855c69da51a15d33d1
sallyasiri7 <sallywork2022@gmail.com>
2021-10-06 21:47:01 +0300

    day9 lab1

---
+++ day8/day8/day8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day8
+//
+//  Created by sally asiri on 29/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

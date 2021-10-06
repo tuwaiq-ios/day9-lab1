4714f74a80d362e356919598147a6923d3046527
sarasaud <unix.663@gmail.com>
2021-10-06 23:30:55 +0300

    ssss

---
+++ day8 Localization/day8 Localization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day8 Localization
+//
+//  Created by SARA SAUD on 2/29/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
